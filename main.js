a1facabacca9fc382f7ee8da5e969add88c6567c
Facu <5675299@mec.edu.py>
2021-03-03 11:39:39 -0300

    finish style of calculator

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 //CALCULAR PORCENTAJE
 const CALCULATEPERCENT = () => {
     let nofp = document.querySelector(".form__input--main").value;
-    let perc = parseFloat(nofp);
-    console.log(perc)
+    if(nofp === "") return document.querySelector(".form__span--result").innerHTML = `Ingrese el porcentaje por favor`;
+    let perc = parseFloat(nofp)
     let pri = document.querySelector(".form__label--input").value;
+    if(pri === "") return document.querySelector(".form__span--result").innerHTML = `Ingrese el precio por favor`;
     let pre = parseFloat(pri);
-    console.log(pre)
+    if(typeof pre !== 'number') return document.querySelector(".form__span--result").innerHTML = `Ingrese el precio por favor`;
     let percen = (perc * pre) / 100;
-    console.log(percen)
-    let pr = pre - perc;
-    console.log(pr)
+    let pr = pre + perc;
     document.querySelector(".form__span--result").innerHTML = `El IVA ES ${percen}`
     document.querySelector(".form__span--result2").innerHTML = `El PRECIO CON IVA ES ${pr}`
 }
